4f479872d78159dc1ba5e452702e6a86ff791ad3
Bo Marchman <bo.marchman@gmail.com>
2022-03-07 20:46:58 -0500

    Configure logging with environment variables

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::env;
 
 use log::error;
 use log::LevelFilter;
-// use log4rs::append::console::{ConsoleAppender, Target};
+use log4rs::append::console::{ConsoleAppender, Target};
 use log4rs::append::file::FileAppender;
 use log4rs::config::{Appender, Config, Root};
 use log_panics;
@@ -57,24 +57,36 @@ enum Command {
 }
 
 fn configure_logging() -> () {
-    //    let stderr = ConsoleAppender::builder().target(Target::Stderr).build();
-
     log_panics::init();
+
+    let log_level = match env::var("LOG_LEVEL") {
+        Ok(l) if l == "DEBUG" => LevelFilter::Debug,
+        Ok(l) if l == "INFO" => LevelFilter::Info,
+        Ok(l) => panic!("Unknown LOG_LEVEL: {}", l),
+        Err(_) => LevelFilter::Info,
+    };
+
+    let log_stdout = env::var("LOG_STDOUT").is_ok();
 
     let file = FileAppender::builder()
         .build("/home/bo/Code/chess-engine/out.log")
         .unwrap();
 
-    let config = Config::builder()
-        //        .appender(Appender::builder().build("stderr", Box::new(stderr)))
-        .appender(Appender::builder().build("file", Box::new(file)))
-        .build(
-            Root::builder()
-                //                .appender("stderr")
-                .appender("file")
-                .build(LevelFilter::Debug),
-        )
-        .unwrap();
+    let stderr = ConsoleAppender::builder().target(Target::Stderr).build();
+
+    let mut config_builder =
+        Config::builder().appender(Appender::builder().build("file", Box::new(file)));
+    if log_stdout {
+        config_builder =
+            config_builder.appender(Appender::builder().build("stderr", Box::new(stderr)));
+    }
+
+    let mut root_builder = Root::builder().appender("file");
+    if log_stdout {
+        root_builder = root_builder.appender("stderr");
+    }
+
+    let config = config_builder.build(root_builder.build(log_level)).unwrap();
 
     log4rs::init_config(config).unwrap();
 }
