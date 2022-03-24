b37581c3cd84118018623c9b03cd639e148a613f
Bo Marchman <bo.marchman@gmail.com>
2022-03-23 22:22:21 -0400

    Benchmark 6 plies deep

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -11,9 +11,18 @@ fn midgame_evaluation(c: &mut Criterion) {
     });
 }
 
+fn midgame_evaluation_6_plies(c: &mut Criterion) {
+    let board =
+        fen::parse("rnbqkbnr/2ppp1pp/1p6/p3PpP1/8/8/PPPP1P1P/RNBQKBNR w KQkq f6 0 5").unwrap();
+
+    c.bench_function("midgame evaluation 6 plies", |b| {
+        b.iter(|| board.clone().find_next_move(black_box(6)).unwrap())
+    });
+}
+
 criterion_group! {
     name = benches;
     config = Criterion::default();
-    targets = midgame_evaluation
+    targets = midgame_evaluation,midgame_evaluation_6_plies
 }
 criterion_main!(benches);
